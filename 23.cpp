0af297113f4f7bda0bac072dbd3d478cfb28b1da
风吹树 <invalid_ms_user@live.com>
2015-10-12 18:58:37 +0800

    to be elegant

--- 23.cpp
+++ 23.cpp
@@ -1,41 +1,54 @@
 // https://leetcode.com/problems/merge-k-sorted-lists/
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode dummy(0);
-        dummy.next = nullptr;
-        ListNode* tail = &dummy;
-        std::vector<ListNode*> listVec = lists;
-        // remove null pointers
-        listVec.erase(std::remove(std::begin(listVec), std::end(listVec), nullptr), std::end(listVec));
-        auto ListNodeCompare = [](const ListNode* lhs, const ListNode* rhs) {
-          return lhs->val < rhs->val;  
-        };
-        auto tailInsert = [&tail](int val) {
-            tail->next = new ListNode(val);
-            tail = tail->next;
-            tail->next = nullptr;
-        };
-        std::multiset<ListNode*, decltype(ListNodeCompare)> listSet(std::begin(listVec), std::end(listVec),
-                                                                ListNodeCompare);
-        
-        while (!listSet.empty()) {
-            ListNode* ptr = *listSet.begin();
-            tailInsert(ptr->val);
-            listSet.erase(listSet.begin());
-            if (ptr->next != nullptr) {
-                listSet.insert(ptr->next);
-            }
-        }
-        return dummy.next;
+  ListNode *mergeKLists(vector<ListNode *> &lists) {
+    auto comp = [](const ListNode *lhs, const ListNode *rhs) {
+      return lhs->val > rhs->val;
+    };
+    std::priority_queue<ListNode *, std::vector<ListNode *>, decltype(comp)>
+        node_queue(std::begin(lists),
+                   std::remove(std::begin(lists), std::end(lists), nullptr),
+                   comp);
+
+    ListNode dummy(0);
+    ListNode *tail = &dummy;
+    while (!node_queue.empty()) {
+      tail->next = node_queue.top();
+      tail = tail->next;
+      node_queue.pop();
+
+      if (tail->next != nullptr) {
+        node_queue.push(tail->next);
+      }
     }
+    tail->next = nullptr;
+    return dummy.next;
+  }
 };
 
+//----------------------------------------------------
+class Solution {
+public:
+  ListNode *mergeKLists(vector<ListNode *> &lists) {
+    auto comp = [](const ListNode *lhs, const ListNode *rhs) {
+      return lhs->val < rhs->val;
+    };
+    std::multiset<ListNode *, decltype(comp)> list_set(
+        std::begin(lists),
+        std::remove(std::begin(lists), std::end(lists), nullptr), comp);
+
+    ListNode dummy(0);
+    ListNode *tail = &dummy;
+    while (!list_set.empty()) {
+      tail->next = *list_set.begin();
+      tail = tail->next;
+
+      list_set.erase(list_set.begin());
+      if (tail->next != nullptr) {
+        list_set.insert(tail->next);
+      }
+    }
+    tail->next = nullptr;
+    return dummy.next;
+  }
+};
