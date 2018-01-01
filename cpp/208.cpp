004cf9cd175a904126479585a50ebf42d8447b40
风 <invalid_ms_user@live.com>
2018-01-01 19:47:42 +0800

    Update 208.cpp

--- cpp/208.cpp
+++ cpp/208.cpp
@@ -1,36 +1,52 @@
-// https://leetcode.com/problems/implement-trie-prefix-tree/
-class TrieNode {
- public:
-  TrieNode* next[26] = {nullptr};
-  bool is_word = false;
-};
-
 class Trie {
- public:
-  Trie() : root(new TrieNode()) {}
-  void insert(const std::string& s) {
-    TrieNode* p = root;
-    for (char ch : s) {
-      if (p->next[ch - 'a'] == nullptr) {
-        p->next[ch - 'a'] = new TrieNode();
-      }
-      p = p->next[ch - 'a'];
+public:
+    struct TrieNode {
+        TrieNode() = default;
+        TrieNode(char ch) : current(ch) {}
+        char current = ' ';
+        bool is_leaf = false;
+        static constexpr size_t kChildrenNum = 26;
+        std::unique_ptr<TrieNode> children[kChildrenNum];
+    };
+    /** Initialize your data structure here. */
+    Trie() {}
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        auto* p = &root;
+        for (auto ch : word) {
+            if (!p->children[ch - 'a']) p->children[ch - 'a'] = std::make_unique<TrieNode>(ch);
+            p = p->children[ch - 'a'].get();
+        }
+        p->is_leaf = true;
     }
-    p->is_word = true;
-  }
-  bool search(const std::string& key) {
-    TrieNode* p = find(key);
-    return p != nullptr && p->is_word;
-  }
-  bool startsWith(const std::string& prefix) { return find(prefix) != nullptr; }
-
- private:
-  TrieNode* find(const std::string& key) {
-    TrieNode* p = root;
-    for (size_t i = 0; i < key.size() && p != nullptr; ++i) {
-      p = p->next[key[i] - 'a'];
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        auto* p = find_to_end(word);
+        return p && p->is_leaf;
     }
-    return p;
-  }
-  TrieNode* root;
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        return find_to_end(prefix) != nullptr;
+    }
+    private:
+    TrieNode* find_to_end(const std::string& str) {
+        auto *p = &root;
+        for (auto ch : str) {
+            if (!p->children[ch - 'a']) return nullptr;
+            p = p->children[ch - 'a'].get();
+        }
+        return p;
+    }
+    TrieNode root;
 };
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * bool param_2 = obj.search(word);
+ * bool param_3 = obj.startsWith(prefix);
+ */
